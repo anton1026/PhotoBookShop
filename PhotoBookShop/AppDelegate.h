63a4859ecfa96d8be64211badc05eeffa746454a
Adrian Rusin <antonborev@anton-mac.local>
2016-02-06 17:55:06 +0200

    Initial Commit

---
+++ PhotoBookShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoBookShop
+//
+//  Created by Anton Borev on 2/6/16.
+//  Copyright © 2016 Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
